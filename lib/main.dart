b0ce3d6ab021b20a77654ee3a4232df659808534
realtnt <theo@thetaphoto.co.uk>
2024-05-01 16:11:47 +0100

    Migrated to a different Firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
+    name: 'flutter_rpg',
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
